6fff8960671477904808594cb7bda48cf4a66442
Praneel <spectralphoenix.07@gmail.com>
2024-02-18 12:02:20 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -7,19 +7,21 @@ from src.summarise import summarise
 
 user_query = st.text_input("Enter the Query: ")
 
-start_time = time.time()
+if st.button("Search"):
+    
+    start_time = time.time()
 
-# Search the Web and Collect Links4
-links = quick_web_search(user_query)
-st.info("Web Search Completed!")
+    # Search the Web and Collect Links4
+    links = quick_web_search(user_query)
+    st.info("Web Search Completed!")
 
-# Scrape the Text from the Links
-text = scrape_text(links)
-answer = summarise(user_query, text)
+    # Scrape the Text from the Links
+    text = scrape_text(links)
+    answer = summarise(user_query, text)
 
-end_time = time.time()
-elapsed_time = "{:.2f}".format(end_time - start_time)
+    end_time = time.time()
+    elapsed_time = "{:.2f}".format(end_time - start_time)
 
-st.markdown(f"\nAnswer: {answer}")
+    st.markdown(f"\nAnswer: {answer}")
 
-st.text(f"Time Elapsed: {elapsed_time} seconds")
+    st.text(f"Time Elapsed: {elapsed_time} seconds")
