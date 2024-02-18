30f2432b82fb8d8be5ca49784331f753fbabe91b
spectralphoenix.07 <spectralphoenix.07@gmail.com>
2024-02-18 06:16:13 +0000

    renamed:    main.py -> app.py 	modified:   requirements.txt

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import time
+import streamlit as st
 
 from src.scrape import scrape_text
 from src.search import quick_web_search, deep_web_search
 from src.summarise import summarise
 
-user_query = input("Enter the Query: ")
+user_query = st.text_input("Enter the Query: ")
 
 start_time = time.time()
 
 # Search the Web and Collect Links4
 links = quick_web_search(user_query)
-print("Web Search Completed!")
+st.info("Web Search Completed!")
 
 # Scrape the Text from the Links
 text = scrape_text(links)
@@ -19,6 +20,6 @@ answer = summarise(user_query, text)
 end_time = time.time()
 elapsed_time = "{:.2f}".format(end_time - start_time)
 
-print(f"\nAnswer: {answer}")
+st.markdown(f"\nAnswer: {answer}")
 
-print(f"Time Elapsed: {elapsed_time} seconds")
+st.text(f"Time Elapsed: {elapsed_time} seconds")
